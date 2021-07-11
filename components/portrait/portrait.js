c617f734b27265d99279f322147485321bc0d924
ismay <ismay@ismaywolff.nl>
2021-07-11 18:04:44 +0200

    Update profile image

--- components/portrait/portrait.js
+++ components/portrait/portrait.js
@@ -1,16 +1,11 @@
-import ResponsiveImage from "../responsive-image";
+import Image from "next/image";
+import profileImage from "../../public/img/portrait.jpg";
 import s from "./portrait.module.css";
 
 export default function Portrait() {
   return (
     <div className={s.container}>
-      <ResponsiveImage
-        alt="Portrait"
-        className={s.image}
-        height={462}
-        src="/img/portrait.jpg"
-        width={460}
-      />
+      <Image alt="Portrait" placeholder="blur" src={profileImage} />
       <p className={s.description}>
         Credit and thanks for the photo to{" "}
         <a href="http://danieloverbeek.com/">Daniel Overbeek</a>
--- components/responsive-image/index.js
+++
@@ -1,3 +0,0 @@
-import ResponsiveImage from "./responsive-image";
-
-export default ResponsiveImage;
