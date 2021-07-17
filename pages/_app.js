60e68c389b0f2d5020ba9a08a9191afe9f78dc2c
ismay <ismay@ismaywolff.nl>
2021-07-17 09:45:46 +0200

    Use custom document

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,11 @@
 /* eslint-disable react/prop-types, react/jsx-props-no-spreading */
 
 import "../styles/globals.css";
-import Head from "next/head";
 import Page from "../components/page";
 
-export default function App({ Component, pageProps }) {
+export default function CustomApp({ Component, pageProps }) {
   return (
     <Page>
-      <Head>
-        <link
-          href="/favicon/favicon-16x16.png"
-          rel="icon"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link
-          href="/favicon/favicon-32x32.png"
-          rel="icon"
-          sizes="32x32"
-          type="image/png"
-        />
-        <script
-          async
-          defer
-          data-dnt="false"
-          data-host="https://microanalytics.io"
-          id="ZwSg9rf6GA"
-          src="https://microanalytics.io/js/script.js"
-        />
-      </Head>
       <Component {...pageProps} />
     </Page>
   );
