898a22d34352859f5b27f52e28d52bbfe9d4c8b4
ismay <ismay@ismaywolff.nl>
2021-07-19 19:52:42 +0200

    Use remote mdx

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
-const withMDX = require("@next/mdx")();
-
-module.exports = withMDX({
+module.exports = {
   reactStrictMode: true,
-});
+};
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,46 @@
-import Head from "next/head";
-import Introduction from "../components/introduction";
+/* eslint-disable react/jsx-props-no-spreading, react/forbid-prop-types */
 
-export default function Home() {
+import { gql } from "graphql-request";
+import { MDXRemote } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import Head from "next/head";
+import T from "prop-types";
+import client from "../client";
+import Portrait from "../components/portrait";
+
+const components = { Portrait };
+
+export default function Home({ source }) {
   return (
     <>
       <Head>
         <title>Wolff Software Engineering</title>
       </Head>
-      <Introduction />
+      <MDXRemote {...source} components={components} />
     </>
   );
+}
+
+Home.propTypes = {
+  source: T.object.isRequired,
+};
+
+export async function getStaticProps() {
+  const { page } = await client.request(
+    gql`
+      query getPage($title: String!) {
+        page(where: { title: $title }) {
+          mdx
+        }
+      }
+    `,
+    { title: "Home" }
+  );
+
+  const source = await serialize(page.mdx);
+
+  return {
+    props: { source },
+    revalidate: 3600,
+  };
 }
