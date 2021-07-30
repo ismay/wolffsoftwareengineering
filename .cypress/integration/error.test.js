66d25f29d0572ab7e98caeaad0fe55894d5ff103
ismay <ismay@ismaywolff.nl>
2021-07-30 12:19:46 +0200

    Remove repetition from cypress tests

--- .cypress/integration/error.test.js
+++ .cypress/integration/error.test.js
@@ -1,19 +1,3 @@
-const links = [
-  { href: "/", name: "Wolff Software Engineering" },
-  { href: "mailto:ismay@wolffsoftwareengineering.nl", name: "Email" },
-  { href: "https://scholar.social/@ismay", name: "Mastodon" },
-  { href: "https://github.com/ismay", name: "Github" },
-  {
-    href: "https://www.linkedin.com/in/wolffsoftwareengineering",
-    name: "Linkedin",
-  },
-];
-
-const terms = [
-  { href: "/pdf/terms-and-conditions.pdf", name: "EN" },
-  { href: "/pdf/algemene-voorwaarden.pdf", name: "NL" },
-];
-
 describe("error", () => {
   it("renders as expected", () => {
     cy.visit("/should-not-exist", { failOnStatusCode: false });
@@ -26,24 +10,6 @@ describe("error", () => {
       url: "/should-not-exist",
     }).then((response) => {
       expect(response.status).to.eq(404);
-    });
-
-    // Ensure expected links are all present
-    links.forEach(({ href, name }) => {
-      cy.findByRole("link", { name })
-        .should("have.attr", "href", href)
-        .and("be.visible");
-    });
-
-    // Ensure the terms and conditions links work
-    terms.forEach(({ href, name }) => {
-      cy.findByRole("link", { name })
-        .should("have.attr", "href", href)
-        .and("be.visible");
-      cy.request(href).then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.headers["content-type"]).to.eq("application/pdf");
-      });
     });
 
     cy.checkA11y();
