cd3b8e5d59b0ef4264638e5a003ff12ca2a2718e
ismay <ismay@ismaywolff.nl>
2021-08-04 19:22:16 +0200

    Fix double pageview

--- hooks/panelbear.js
+++ hooks/panelbear.js
@@ -2,13 +2,11 @@ import * as Panelbear from "@panelbear/panelbear-js";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-const defaultConfig = {};
-
-export default function usePanelbear(site, config = defaultConfig) {
+export default function usePanelbear(site) {
   const router = useRouter();
 
   useEffect(() => {
-    Panelbear.load(site, config);
+    Panelbear.load(site, { autoTrack: false });
     Panelbear.trackPageview();
 
     const handleRouteChange = () => Panelbear.trackPageview();
@@ -17,5 +15,5 @@ export default function usePanelbear(site, config = defaultConfig) {
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
-  }, [config, router.events, site]);
+  }, [router.events, site]);
 }
