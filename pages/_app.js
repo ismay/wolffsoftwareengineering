4ae16732952cdd9f451469f5f6108ba1b6612281
ismay <ismay@ismaywolff.nl>
2021-09-27 23:17:18 +0200

    Update panelbear

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types, react/jsx-props-no-spreading, global-require */
 
 import "../styles/globals.css";
+import { usePanelbear } from "@panelbear/panelbear-nextjs";
 import Page from "../components/page";
-import usePanelbear from "../hooks/panelbear";
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
   require("../.msw");
